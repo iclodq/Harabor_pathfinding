d372d6cb824732191fe679282693e26d022db4b9
Edward Lam <ed@ed-lam.com>
2019-02-02 07:58:30 +1100

    Use sorted vector for time constraints.

--- src/mapf/time_constraints.h
+++ src/mapf/time_constraints.h
@@ -35,7 +35,6 @@ struct cell_constraint
 {
     cell_constraint()
     {
-        v_ = 0;
         e_[warthog::cbs::move::NORTH] = 1;
         e_[warthog::cbs::move::SOUTH] = 1;
         e_[warthog::cbs::move::EAST] = 1;
@@ -47,7 +46,6 @@ struct cell_constraint
     cell_constraint&
     operator=(const cell_constraint& other)
     {
-        v_ = other.v_;
         e_[0] = other.e_[0];
         e_[1] = other.e_[1];
         e_[2] = other.e_[2];
@@ -57,7 +55,6 @@ struct cell_constraint
         return *this;
     }
 
-    bool v_;
     double e_[5];
     uint16_t timestep_;
 };
@@ -67,106 +64,156 @@ class time_constraints
 {
     public:
 
-       time_constraints(uint32_t map_xy_sz) : map_xy_sz_(map_xy_sz)
-       {
-           cons_ = new std::vector< std::vector<CONSTRAINT> >(map_xy_sz_);
-       } 
+       time_constraints() noexcept = delete;
+       time_constraints(const uint32_t map_xy_sz) noexcept : cons_(map_xy_sz) {}
+       ~time_constraints() noexcept = default;
 
-       ~time_constraints()
+       time_constraints(const time_constraints& other) = default;
+       time_constraints(time_constraints&& other) noexcept = default;
+       time_constraints& operator=(const time_constraints& other) = default;
+       time_constraints& operator=(time_constraints&& other) noexcept = default;
+
+       // get size
+       inline uint32_t
+       map_xy_sz() const
        {
-           delete cons_;
+           return cons_.size();
        }
 
        // add a constraint to location @param xy_id
        inline void
-       add_constraint(uint32_t xy_id, CONSTRAINT& con)
+       add_or_replace_constraint(const uint32_t xy_id, const CONSTRAINT& con)
        {
-           assert(xy_id < cons_->size());
-           CONSTRAINT* cur_con = 
-               get_constraint(xy_id, con.timestep_);
-           if(!cur_con)
+           auto& xy_cons = cons_[xy_id];
+           auto it = find_pos(xy_id, con.t);
+           if (it != xy_cons.end() && it->timestep_ == con.timestep_)
            {
-               cons_->at(xy_id).push_back(con);
+               *it = con;
            }
            else
            {
-                *cur_con = con;
+               xy_cons.insert(it, con);
            }
        }
 
        // return all constraints associated with the xy location
        // @param padded_id
-       inline std::vector<CONSTRAINT>& 
-       get_constraint_set(uint32_t xy_id)
+       inline std::vector<CONSTRAINT>&
+       get_constraint_set(const uint32_t xy_id)
        {
-           return cons_->at(xy_id);
+#ifndef NDEBUG
+           assert(xy_id < map_xy_sz());
+#endif
+           return cons_[xy_id];
        }
 
-       // return the constraint associated with the location 
+       // return the constraint associated with the location
        // @param xy_id, at the time @param timestep
        inline CONSTRAINT*
        get_constraint(uint32_t xy_id, uint32_t timestep)
        {
-            auto retval = 
-                std::find_if(
-                        (*cons_)[xy_id].begin(),
-                        (*cons_)[xy_id].end(),
-                    [timestep](CONSTRAINT& tmp)
-                    -> bool
-                    {
-                        return tmp.timestep_  == timestep;
-                    });
-            if(retval == (*cons_)[xy_id].end())
-            {
-                return 0;
-            }
-            return &*retval;
+           CONSTRAINT* con = nullptr;
+
+           auto& xy_cons = cons_[xy_id];
+           auto it = find_pos(xy_id, timestep);
+           if (it != xy_cons.end() && it->timestep_ == timestep)
+               con = &*it;
+
+           return con;
        }
 
        // create or return the constraint associated with the location
        // @param xy_id, at the time @param timestep
-       inline CONSTRAINT*
+       inline CONSTRAINT&
        get_or_create_constraint(uint32_t xy_id, uint32_t timestep)
        {
-            auto retval =
-                std::find_if(
-                        (*cons_)[xy_id].begin(),
-                        (*cons_)[xy_id].end(),
-                    [timestep](CONSTRAINT& tmp)
-                    -> bool
-                    {
-                        return tmp.timestep_  == timestep;
-                    });
-            if(retval == (*cons_)[xy_id].end())
-            {
-                auto* con = &((*cons_)[xy_id].emplace_back());
-                con->timestep_ = timestep;
-                return con;
-            }
-            return &*retval;
+           CONSTRAINT* con;
+
+           auto& xy_cons = cons_[xy_id];
+           auto it = find_pos(xy_id, timestep);
+           if (it != xy_cons.end() && it->timestep_ == timestep)
+           {
+               con = &*it;
+           }
+           else
+           {
+               con = &*xy_cons.emplace(it);
+               con->timestep_ = timestep;
+           }
+
+           return *con;
        }
 
        // remove all constraints associated with location
-       void
-       clear_constraint_set(uint32_t xy_id)
+       inline void
+       clear_constraint_at(const uint32_t xy_id)
        {
-           cons_->at(xy_id).clear();
+           auto& xy_cons = cons_[xy_id];
+           xy_cons.clear();
        }
 
        // remove all constraints
-       void
+       inline void
        clear()
        {
-           for(uint32_t i = 0; i < cons_->size(); i++)
+           for (auto& xy_cons : cons_)
+               xy_cons.clear();
+       }
+
+       // remove all constraints with default values
+       void
+       clean_up()
+       {
+           for (auto& xy_cons : cons_)
            {
-               cons_->at(i).clear();
-           }   
+               bool deleted = false;
+               for (auto it = xy_cons.begin(); it != xy_cons.end();)
+               {
+                   auto& con = *it;
+                   if (con.e_[warthog::cbs::move::NORTH] == 1 &&
+                       con.e_[warthog::cbs::move::SOUTH] == 1 &&
+                       con.e_[warthog::cbs::move::EAST] == 1 &&
+                       con.e_[warthog::cbs::move::WEST] == 1 &&
+                       con.e_[warthog::cbs::move::WAIT] == 1)
+                   {
+                       std::swap(*it, xy_cons.back());
+                       xy_cons.pop_back();
+                       deleted = true;
+                   }
+                   else
+                   {
+                       ++it;
+                   }
+               }
+               if (deleted)
+               {
+                   std::sort(xy_cons.begin(),
+                             xy_cons.end(),
+                             [](const CONSTRAINT& a, const CONSTRAINT& b)
+                             { return a.timestep_ < b.timestep_; });
+               }
+           }
        }
 
     private:
-        std::vector< std::vector< CONSTRAINT > >* cons_;
-        CONSTRAINT dummy_;
-        uint32_t map_xy_sz_;
+        std::vector<std::vector<CONSTRAINT>> cons_;
+
+        inline auto
+        find_pos(const uint32_t xy_id, const uint16_t t)
+        {
+#ifndef NDEBUG
+            assert(xy_id < map_xy_sz());
+#endif
+            auto& xy_cons = cons_[xy_id];
+            auto it = std::lower_bound(xy_cons.begin(),
+                                       xy_cons.end(),
+                                       t,
+                                       [](const CONSTRAINT& con, const uint16_t t)
+                                       {
+                                           return con.timestep_ < t;
+                                       });
+            return it;
+        }
 
 }; // time_constraints
 
