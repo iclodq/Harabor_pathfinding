0df961a41bc2cd05642b5bcd56562e7ae3309eab
Edward Lam <ed@ed-lam.com>
2019-03-15 12:53:56 +1100

    Use new data structure for reservation table.

--- src/mapf/reservation_table.h
+++ src/mapf/reservation_table.h
@@ -26,30 +26,39 @@
 #include <stdint.h>
 #include <vector>
 #include <cstring>
+#include <cmath>
 
 namespace warthog
 {
 
 class reservation_table
 {
-    static const uint32_t LOG2_QWORD_SZ = 6;
     public:
-        reservation_table(uint32_t map_sz) : map_sz_(map_sz) 
+        reservation_table(const uint32_t map_sz) :
+            table_(reinterpret_cast<char*>(
+                        std::malloc((4 * sqrt(map_sz)) * map_sz / CHAR_BIT + 1))),
+            timesteps_(4 * sqrt(map_sz)),
+            map_sz_(map_sz)
         {
-            map_sz_in_qwords_ = (map_sz_ >> LOG2_QWORD_SZ)+1;
-            pool_ = new warthog::mem::cpool(map_sz_in_qwords_);
+            clear_reservations();
         }
-        ~reservation_table() 
+        ~reservation_table() noexcept
         {
-            delete pool_;
+            std::free(table_);
         }
 
         inline bool
-        is_reserved(uint32_t xy_id, uint32_t timestep)
+        is_reserved(const uint32_t xy_id, const int32_t timestep)
         {
-            if(timestep >= table_.size()) { return false; }
-            return table_[timestep][xy_id >> LOG2_QWORD_SZ] & 
-                   (1 << (xy_id & 63));
+            if (timestep >= timesteps_)
+            {
+                return false;
+            }
+
+            const auto elem = timestep * map_sz_ + xy_id;
+            const auto idx = elem / CHAR_BIT;
+            const char mask = 0x1 << (elem % CHAR_BIT);
+            return (table_[idx] & mask) != 0x0;
         }
 
         inline bool
@@ -61,16 +70,27 @@ class reservation_table
         }
 
         inline void
-        reserve(uint32_t xy_id, uint32_t timestep)
+        reserve(const uint32_t xy_id, const int32_t timestep)
         {
             assert(xy_id < map_sz_);
-            while(table_.size() <= timestep)
+
+            if (timestep >= timesteps_)
             {
-                uint64_t* map = (uint64_t*)pool_->allocate();
-                memset(map, 0, sizeof(uint64_t) * map_sz_in_qwords_);
-                table_.push_back(map);
+                const auto new_timesteps = timestep + 50;
+                table_ = reinterpret_cast<char*>(
+                    std::realloc(table_, new_timesteps * map_sz_ / CHAR_BIT + 1));
+                if (!table_)
+                {
+                    printf("Failed to reallocate memory for reservation table");
+                    std::abort();
+                }
+                timesteps_ = new_timesteps;
             }
-            table_[timestep][xy_id >> LOG2_QWORD_SZ] |= (1 << (xy_id & 63));
+
+            const auto elem = timestep * map_sz_ + xy_id;
+            const auto idx = elem / CHAR_BIT;
+            const char mask = 0x1 << (elem % CHAR_BIT);
+            table_[idx] |= mask;
         }
 
         inline void
@@ -82,11 +102,15 @@ class reservation_table
         }
 
         inline void
-        unreserve(uint32_t xy_id, uint32_t timestep)
+        unreserve(const uint32_t xy_id, const int32_t timestep)
         {
-            assert(timestep < table_.size());
             assert(xy_id < map_sz_);
-            table_[timestep][xy_id >> LOG2_QWORD_SZ] &= ~(1 << (xy_id & 63));
+            assert(timestep < timesteps_);
+
+            const auto elem = timestep * map_sz_ + xy_id;
+            const auto idx = elem / CHAR_BIT;
+            const char mask = 0x1 << (elem % CHAR_BIT);
+            table_[idx] &= ~mask;
         }
 
         inline void
@@ -100,18 +124,13 @@ class reservation_table
         inline void
         clear_reservations()
         {
-            for(uint64_t* map : table_)
-            {
-                memset(map, 0, sizeof(uint64_t) * map_sz_in_qwords_);
-            }
+            memset(table_, 0, timesteps_ * map_sz_ / CHAR_BIT + 1);
         }
 
     private:
-        std::vector<uint64_t*> table_;
-        uint32_t map_sz_;
-        uint32_t map_sz_in_qwords_;
-        warthog::mem::cpool* pool_;
-
+        char* table_;
+        int32_t timesteps_;
+        const uint32_t map_sz_;
 
 };
 
